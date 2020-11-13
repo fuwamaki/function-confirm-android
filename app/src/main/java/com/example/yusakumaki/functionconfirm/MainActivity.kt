c0f22399cab0cfba9f0d2e1040d9bfa18c1d5d89
fuwamaki <yusaku.maki@street-academy.com>
2020-11-13 10:41:12 +0900

    add fragment

--- app/src/main/java/com/example/yusakumaki/functionconfirm/MainActivity.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.yusakumaki.functionconfirm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = MainFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.container, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.example.yusakumaki.functionconfirm
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
