2dbf07bcee701a0e92e5eec66c43e88ae9f0443e
fuwamaki <yusaku.maki@street-academy.com>
2020-11-19 18:05:49 +0900

    set material motion segue

--- app/src/main/java/com/example/yusakumaki/functionconfirm/MainFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
@@ -14,7 +15,8 @@ class MainFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         view.main_sample_button.setOnClickListener {
-            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+            val extras = FragmentNavigatorExtras(view to "shared_element_container")
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment, null, null, extras)
         }
         return view
     }
--- app/src/main/java/com/example/yusakumaki/functionconfirm/SecondFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/SecondFragment.kt
@@ -5,8 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.transition.MaterialContainerTransform
 
 class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val transform = MaterialContainerTransform()
+        transform.duration = 1000
+        sharedElementEnterTransition = transform
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
