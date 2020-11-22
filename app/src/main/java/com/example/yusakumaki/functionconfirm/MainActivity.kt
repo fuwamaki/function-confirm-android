672cf8e48f2a01ea5869d0014b04c4556f1b2897
fuwamaki <yusaku.maki@street-academy.com>
2020-11-22 20:27:18 +0900

    setup bottom navigation

--- app/src/main/java/com/example/yusakumaki/functionconfirm/MainActivity.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.yusakumaki.functionconfirm
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.*
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +16,59 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        val fragment = MainFragment()
-//        val fragmentTransaction = supportFragmentManager.beginTransaction()
-//        fragmentTransaction.add(R.id.container, fragment)
-//        fragmentTransaction.commit()
+        val navController = findNavController(R.id.main_navigation_host)
+        NavigationUI.setupWithNavController(bottom_navigation, navController)
+    }
+}
+
+@Navigator.Name("custom_fragment")
+class CustomNavigator(
+        private val context: Context,
+        private val manager: FragmentManager,
+        private val containerId: Int
+) : FragmentNavigator(context, manager, containerId) {
+
+    @Suppress("DEPRECATION")
+    override fun navigate(
+            destination: Destination,
+            args: Bundle?,
+            navOptions: NavOptions?,
+            navigatorExtras: Navigator.Extras?
+    ): NavDestination? {
+        if (manager.isStateSaved) {
+            return null
+        }
+
+        var className = destination.className
+        if (className[0] == '.') {
+            className = context.packageName + className
+        }
+
+        val tag = destination.id.toString()
+        val transaction = manager.beginTransaction()
+
+        val currentFragment = manager.primaryNavigationFragment
+        if (currentFragment != null) {
+            transaction.hide(currentFragment)
+        }
+
+        var fragment = manager.findFragmentByTag(tag)
+        if (fragment == null) {
+            fragment = instantiateFragment(context, manager, className, args)
+            transaction.add(containerId, fragment, tag)
+        }
+        fragment.arguments = args
+
+        transaction.show(fragment)
+        transaction.setPrimaryNavigationFragment(fragment)
+        transaction.commit()
+
+        return destination
+    }
+}
+
+class CustomNavHostFragment : NavHostFragment() {
+    override fun createFragmentNavigator(): Navigator<out FragmentNavigator.Destination> {
+        return CustomNavigator(requireContext(), childFragmentManager, id)
     }
 }
--- app/src/main/java/com/example/yusakumaki/functionconfirm/MainFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/MainFragment.kt
@@ -7,16 +7,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import com.example.yusakumaki.functionconfirm.databinding.FragmentMainBinding
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
 class MainFragment : Fragment() {
 
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        // setup binding
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        // setup view
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         view.main_sample_button.setOnClickListener {
             val extras = FragmentNavigatorExtras(view to "shared_element_container")
-            findNavController().navigate(R.id.action_firstFragment_to_secondFragment, null, null, extras)
+            findNavController().navigate(R.id.show_second_fragment, null, null, extras)
         }
         return view
     }
