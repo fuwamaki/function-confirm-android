57141662dd857a67a80c72bc818efe3cd357456a
fuwamaki <yusaku.maki@street-academy.com>
2021-02-17 16:08:09 +0900

    add sample needsStrikeThrough

--- app/src/main/java/com/example/yusakumaki/functionconfirm/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/Fragments/ProfileFragment.kt
@@ -1,16 +1,28 @@
 package com.example.yusakumaki.functionconfirm.Fragments
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.yusakumaki.functionconfirm.R
+import com.example.yusakumaki.functionconfirm.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val needsStrikeThrough = true
+        binding.sampleTextView.apply {
+            paint.flags = if (needsStrikeThrough) paintFlags or Paint.STRIKE_THRU_TEXT_FLAG else Paint.ANTI_ALIAS_FLAG
+            paint.isAntiAlias = needsStrikeThrough
+            invalidate() // 再描画
+        }
+        return binding.root
     }
 }
