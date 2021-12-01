34eb9aed56d1f983d4306532bb233d2bc002bd3d
fuwamaki <variety.fuwa.3@gmail.com>
2021-12-01 13:42:02 +0900

    fix

--- app/src/test/java/com/example/yusakumaki/functionconfirm/ProfileViewModelTest.kt
+++ app/src/test/java/com/example/yusakumaki/functionconfirm/ProfileViewModelTest.kt
@@ -1,21 +1,15 @@
 package com.example.yusakumaki.functionconfirm
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.mockito.kotlin.mock
 import com.example.yusakumaki.functionconfirm.fragments.ProfileViewModel
-import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.kotlin.verify
 
 @RunWith(AndroidJUnit4::class)
 class ProfileViewModelTest {
-
-    @Rule
-    @JvmField
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun testOnAction() {
