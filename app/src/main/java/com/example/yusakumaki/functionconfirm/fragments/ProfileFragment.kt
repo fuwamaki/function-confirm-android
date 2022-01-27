fa2ed33ed4851dce57b7db816d31b282c7043c39
fuwamaki <variety.fuwa.3@gmail.com>
2022-01-28 00:38:02 +0900

    add spinner view

--- app/src/main/java/com/example/yusakumaki/functionconfirm/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/fragments/ProfileFragment.kt
@@ -2,12 +2,17 @@ package com.example.yusakumaki.functionconfirm.fragments
 
 import android.graphics.Paint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.yusakumaki.functionconfirm.R
 import com.example.yusakumaki.functionconfirm.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -33,9 +38,29 @@ class ProfileFragment : Fragment() {
             paint.isAntiAlias = needsStrikeThrough
             invalidate() // 再描画
         }
-        viewModel.state.observe(viewLifecycleOwner, {
+        viewModel.state.observe(viewLifecycleOwner) {
             Toast.makeText(context, "tap action", Toast.LENGTH_SHORT).show()
-        })
+        }
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.view_sample_spinner_item,
+            arrayOf(1, 2, 3, 4, 5)
+        )
+        binding.sampleSpinner.adapter = adapter
+        binding.sampleSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val item = (parent as Spinner).selectedItem as Int
+                    Log.v("tag", item.toString())
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
         return binding.root
     }
 }
