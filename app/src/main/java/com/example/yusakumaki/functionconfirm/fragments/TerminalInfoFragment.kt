13e80946e60a30f373e442826ec09add8d51329b
fuwamaki <variety.fuwa.3@gmail.com>
2023-09-06 17:29:25 +0900

    add location permission request in TerminalInfoFragment

--- app/src/main/java/com/example/yusakumaki/functionconfirm/fragments/TerminalInfoFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/fragments/TerminalInfoFragment.kt
@@ -1,9 +1,11 @@
 package com.example.yusakumaki.functionconfirm.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.yusakumaki.functionconfirm.databinding.FragmentTerminalInfoBinding
@@ -15,6 +17,12 @@ class TerminalInfoFragment : Fragment() {
 
     private val viewModel: TerminalInfoViewModel by lazy {
         ViewModelProvider(this).get(TerminalInfoViewModel::class.java)
+    }
+
+    private val locationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { grant ->
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +41,14 @@ class TerminalInfoFragment : Fragment() {
         binding = FragmentTerminalInfoBinding.inflate(inflater, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        // 位置情報の認証リクエスト
+        locationPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
 
         viewModel.requestAdvertisingId()
         viewModel.fetchGlobalIPAddress()
