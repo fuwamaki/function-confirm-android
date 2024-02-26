75e5ac781d6f1521e7aa5a1f3ae8bfc395dc0fe5
fuwamaki <variety.fuwa.3@gmail.com>
2024-02-26 13:56:59 +0900

    adjustment animation

--- app/src/main/java/com/example/yusakumaki/functionconfirm/fragments/CardPagesFragment.kt
+++ app/src/main/java/com/example/yusakumaki/functionconfirm/fragments/CardPagesFragment.kt
@@ -22,29 +22,39 @@ import timber.log.Timber
 import kotlin.math.abs
 
 class CardPagesFragment : Fragment(), GestureDetector.OnGestureListener {
+    companion object {
+        private const val TAKE_CARD_DURATION = 200L
+        private const val BRING_CHILD_TO_BACK_DURATION = 100L
+        private const val BRING_CHILD_TO_BACK_DELAY = 210L
+        private const val MIN_SWIPE_DISTANCE_X = 100
+        private const val MIN_SWIPE_DISTANCE_Y = 100
+        private const val MAX_SWIPE_DISTANCE_X = 2000
+        private const val MAX_SWIPE_DISTANCE_Y = 2000
+    }
+
     enum class CardPageType {
-        first, second, third, fourth
+        First, Second, Third, Fourth
     }
 
     private lateinit var binding: FragmentCardPagesBinding
     private lateinit var mDetector: GestureDetectorCompat
     private var cardPageTypes: Array<CardPageType> = arrayOf(
-        CardPageType.first,
-        CardPageType.second,
-        CardPageType.third,
-        CardPageType.fourth
+        CardPageType.First,
+        CardPageType.Second,
+        CardPageType.Third,
+        CardPageType.Fourth
     )
 
     private val viewModel: CardPagesViewModel by lazy {
         ViewModelProvider(this)[CardPagesViewModel::class.java]
     }
 
-    fun cardView(cardPageType: CardPageType): View {
+    private fun cardView(cardPageType: CardPageType): View {
         return when (cardPageType) {
-            CardPageType.first -> binding.sampleView
-            CardPageType.second -> binding.sampleView2
-            CardPageType.third -> binding.sampleView3
-            CardPageType.fourth -> binding.sampleView4
+            CardPageType.First -> binding.sampleView
+            CardPageType.Second -> binding.sampleView2
+            CardPageType.Third -> binding.sampleView3
+            CardPageType.Fourth -> binding.sampleView4
         }
     }
 
@@ -67,111 +77,60 @@ class CardPagesFragment : Fragment(), GestureDetector.OnGestureListener {
         viewModel.onCreate()
 
         binding.button.setOnClickListener {
-            ObjectAnimator.ofFloat(binding.sampleView, View.ALPHA, 1.0f, 0.0f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView, View.ROTATION, 0f, -5f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView, "translationX", -20f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView2, View.ROTATION, 5f, 0f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView2, "translationX", -20f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView3, View.ROTATION, 10f, 5f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView3, "translationX", -20f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView4, View.ROTATION, 15f, 10f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
-
-            ObjectAnimator.ofFloat(binding.sampleView4, "translationX", -20f).apply {
-                duration = 300
-                interpolator = AccelerateInterpolator()
-                start()
-            }
+            swipeLeftAnimation()
         }
 
         return binding.root
     }
 
     private var isAnimationRunning = false
+
     private fun swipeLeftAnimation() {
         if (isAnimationRunning) return
         isAnimationRunning = true
         // first: fade out
         ObjectAnimator.ofFloat(cardView(cardPageTypes[0]), View.ALPHA, 1.0f, 0.0f).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // first: rotate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[0]), View.ROTATION, 0f, -5f).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // first: translate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[0]), "translationX", cardView(cardPageTypes[0]).translationX-resources.getDimension(R.dimen.size_25)).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // first: 動作後、一番裏に移動
         ObjectAnimator.ofFloat(cardView(cardPageTypes[0]), View.ALPHA, 0.0f, 1.0f).apply {
-            duration = 0
-            startDelay = 320
+            duration = BRING_CHILD_TO_BACK_DURATION
+            startDelay = BRING_CHILD_TO_BACK_DELAY
             start()
         }
         ObjectAnimator.ofFloat(cardView(cardPageTypes[0]), View.ROTATION, -5f, 15f).apply {
             duration = 0
-            startDelay = 300
+            startDelay = TAKE_CARD_DURATION
             start()
         }
         ObjectAnimator.ofFloat(cardView(cardPageTypes[0]), "translationX", cardView(cardPageTypes[0]).translationX+resources.getDimension(R.dimen.size_25)*3).apply {
             duration = 0
-            startDelay = 300
+            startDelay = TAKE_CARD_DURATION
             start()
         }
         CoroutineScope(Dispatchers.Main).launch {
-            delay(300)
+            delay(TAKE_CARD_DURATION)
             binding.cardsFrameLayout.bringChildToFront(cardView(cardPageTypes[3]))
             binding.cardsFrameLayout.bringChildToFront(cardView(cardPageTypes[2]))
             binding.cardsFrameLayout.bringChildToFront(cardView(cardPageTypes[1]))
-            delay(300)
+            delay(100)
             cardPageTypes = arrayOf(
                 cardPageTypes[1],
                 cardPageTypes[2],
@@ -183,35 +142,35 @@ class CardPagesFragment : Fragment(), GestureDetector.OnGestureListener {
 
         // second: rotate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[1]), View.ROTATION, 5f, 0f).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // second: translate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[1]), "translationX", cardView(cardPageTypes[1]).translationX-resources.getDimension(R.dimen.size_25)).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // third: rotate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[2]), View.ROTATION, 10f, 5f).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // third: translate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[2]), "translationX", cardView(cardPageTypes[2]).translationX-resources.getDimension(R.dimen.size_25)).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
 
         // fourth: rotate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[3]), View.ROTATION, 15f, 10f).apply {
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
@@ -219,7 +178,7 @@ class CardPagesFragment : Fragment(), GestureDetector.OnGestureListener {
         // fourth: translate
         ObjectAnimator.ofFloat(cardView(cardPageTypes[3]), "translationX", cardView(cardPageTypes[3]).translationX-resources.getDimension(R.dimen.size_25)).apply {
 
-            duration = 300
+            duration = TAKE_CARD_DURATION
             interpolator = AccelerateInterpolator()
             start()
         }
@@ -250,11 +209,6 @@ class CardPagesFragment : Fragment(), GestureDetector.OnGestureListener {
 
     override fun onFling(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
         Timber.d("OnGestureListener: onFling")
-        val MIN_SWIPE_DISTANCE_X = 100
-        val MIN_SWIPE_DISTANCE_Y = 100
-        val MAX_SWIPE_DISTANCE_X = 2000
-        val MAX_SWIPE_DISTANCE_Y = 2000
-
         val deltaX: Float = p0.x - p1.x
         val deltaY: Float = p0.y - p1.y
         val deltaXAbs = abs(deltaX)
